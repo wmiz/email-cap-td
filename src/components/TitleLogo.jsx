54c68ee3e3ae448fe239b75094a80819e13d86c1
wmiz <willmiswebdev@gmail.com>
2023-06-19 16:27:31 -0500

    Fix copy

--- src/components/TitleLogo.jsx
+++ src/components/TitleLogo.jsx
@@ -6,25 +6,20 @@ const TitleLogo = () => {
   return (
     <>
       <h2 className="text-white w-full font-bold font-goog text-4xl my-2">
-        <span className="bg-black text-white ">
-          <FontAwesomeIcon
-            icon={faEnvelope}
-            className="text-white text-4xl mx-2"
-          />
-          Mastering Shopify:
-        </span>{" "}
-        Tips & Tricks Delivered to Your Inbox
+        <span className="bg-black text-white pl-2 pr-2">{`{}`}</span>
+        <span className="bg-black text-white pr-2 mr-2">ShopSavvy:</span>
+        Mastering Ecom One Byte at a Time
       </h2>
       <p className="text-white w-full text-2xl my-2">
-        Get the weekly email that makes running your Shopify store a breeze.
+        Get the weekly email that makes creating Shopify stores a 💨breeze💨.
       </p>
       <ul className="w-full text-white text-xl my-2">
         <li><FontAwesomeIcon icon={faSquareCheck} className="mr-2"/>
-          Shopify News, Tips, & Tricks</li>
+          Shopify Development News, Tips, & Tricks</li>
           <li><FontAwesomeIcon icon={faSquareCheck} className="mr-2"/>
-          Recommended Shopify Apps</li>
+          App Recommendations</li>
           <li><FontAwesomeIcon icon={faSquareCheck} className="mr-2"/>
-          & More!</li>
+          & More...</li>
       </ul>
     </>
   );
